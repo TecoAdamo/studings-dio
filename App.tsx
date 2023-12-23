c507d284934fbbef5d33336733dd2f506e2b2556
Mateus Adamo <adamomateus478@gmail.com>
2023-12-22 23:38:47 -0300

    feat: .

--- App.tsx
+++ App.tsx
@@ -1,26 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Alert } from 'react-native';
+import Home from "./src/screens/initial/home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View
-        onTouchStart={(event) => {
-          Alert.alert('Toque', 'Toque inicializado!')
-        }}
-      >
-        <Text>Hello World!</Text>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <Home />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
